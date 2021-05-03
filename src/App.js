76cae51904df8d862113e1fb4cea1ae7ea7b474a
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-05-03 19:26:20 +0900

    first establishment

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome To My Library!</h1>
+        <h2>Have a look this place to explore the story.</h2>
+        
+      </div>
+    )
+  }
+}
+
+export default App;
